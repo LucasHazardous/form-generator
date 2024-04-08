c42a0959bd77777535a6d1813b8e1148cd0257cf
LucasHazardous <70599201+LucasHazardous@users.noreply.github.com>
2024-04-08 10:33:46 +0200

    form thing component

--- src/app/form-thing/form-thing.component.ts
+++ src/app/form-thing/form-thing.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormThingService } from '../form-thing.service';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { DynamicForm } from '../../model/form';
 
 @Component({
   selector: 'app-form-thing',
-  standalone: true,
-  imports: [],
   templateUrl: './form-thing.component.html',
   styleUrl: './form-thing.component.css'
 })
-export class FormThingComponent {
+export class FormThingComponent implements OnInit {
+  form: FormGroup = new FormGroup([]);
+  data: DynamicForm = {};
+  Object = Object;
 
+  constructor(private fb: FormBuilder, private formThingService: FormThingService) { }
+
+  ngOnInit() {
+    this.formThingService.fetchForm().subscribe((data: DynamicForm) => {
+      this.data = data;
+
+      this.form = this.fb.group(
+        Object.keys(this.data).reduce((obj: any, item: string) => {
+          obj[item] = new FormControl('');
+          return obj;
+        }, {})
+      );
+    });
+  }
+
+  onSubmit() {
+    this.formThingService.sendForm(
+      Object.keys(this.data).reduce((obj: any, item: string) => {
+        obj[item] = this.form.get(item)!.value;
+        return obj;
+      }, {})
+    );
+  }
 }
