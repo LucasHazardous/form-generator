841ac73d6225e54626f08c84030322889d4199f3
LucasHazardous <70599201+LucasHazardous@users.noreply.github.com>
2024-04-08 10:34:04 +0200

    form thing service

--- src/app/form-thing.service.ts
+++ src/app/form-thing.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { DynamicForm } from '../model/form';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FormThingService {
+  api = "http://localhost:3000/form"
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  fetchForm(): Observable<DynamicForm> {
+    return this.http.get<DynamicForm>(this.api);
+  }
+
+  sendForm(body: any) {
+    console.log(body)
+    this.http.post(this.api, body).subscribe();
+  }
 }
