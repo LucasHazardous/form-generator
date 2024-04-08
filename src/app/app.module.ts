83eeb313243d2f88dd11f8e4dcd69ec42151857e
LucasHazardous <70599201+LucasHazardous@users.noreply.github.com>
2024-04-08 10:34:38 +0200

    convert app to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { AppComponent } from './app.component';
+import { FormThingComponent } from './form-thing/form-thing.component';
+import { FormThingService } from './form-thing.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [],
+  declarations: [AppComponent, FormThingComponent],
+  bootstrap: [AppComponent],
+  providers: [FormThingService],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    BrowserModule,
+    HttpClientModule
   ]
 })
 export class AppModule { }
